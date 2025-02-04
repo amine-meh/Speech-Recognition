99591290206e0f985cada9959877763490d5e917
amine-meh <132357988+amine-meh@users.noreply.github.com>
2025-02-04 15:55:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ r = sr.Recognizer()
 
 
 def record_audio(ask=False):
-  if ask:
-    assistant_speak(ask)
   with sr.Microphone() as source:
+    if ask:
+      assistant_speak(ask)
+
     audio = r.listen(source)
     voice_data = ''
     try:
@@ -22,15 +23,16 @@ def record_audio(ask=False):
       assistant_speak('Sorry, I did not get that')
     except sr.RequestError:
       assistant_speak('Sorry, the service is down')
-    return voice_data
+    print(f">> {voice_data.lower()}")
+    return voice_data.lower()
   
 def assistant_speak(audio_string):
-  tts = gTTS(text = audio_string)
-  r = random.randint(1, 1000000)
-  audio_file = 'audio-' + str(r) + '.mp3'
+  tts = gTTS(text = audio_string, lang='en')
+  r = random.randint(1, 100000000)
+  audio_file = 'audio' + str(r) + '.mp3'
   tts.save(audio_file)
   playsound3.playsound(audio_file)
-  print(audio_string)
+  print(f"Elena: {audio_string}")
   os.remove(audio_file)
   
 def respond(user_req):
@@ -40,35 +42,35 @@ def respond(user_req):
   if 'What time is it' in user_req:
     assistant_speak(ctime())
 
-  if 'search' in user_req:
+  if 'google' in user_req:
     search = record_audio('What do you want to search for?')
     url = 'https://google.com/search?q=' + search
     webbrowser.get().open(url)
-    assistant_speak('Here is what I found for'+ search + ':')
+    assistant_speak('Here is what I found for '+ search + ':')
 
   if 'youtube' in user_req:
     yt_search = record_audio('What do you want to search for on youtube?')
     url = 'https://www.youtube.com/results?search_query=' + yt_search
     webbrowser.get().open(url)
-    assistant_speak('Here is what I found for'+ yt_search + ':')
+    assistant_speak('Here is what I found for '+ yt_search + ':')
 
   if 'location' in user_req:
     location = record_audio('What is the location?')
     url = 'https://google.nl/maps/place/' + location + '/&amp;'
     webbrowser.get().open(url)
-    assistant_speak('Here is the location of'+ location + ':')
+    assistant_speak('Here is the location of '+ location + ':')
 
   if 'exit' in user_req:
     assistant_speak('Goodbye')
     exit()
 
 
-def main():
-  time.sleep(1)
-  assistant_speak('How can I help you?')
-  while 1:
-    voice_data = record_audio()
-    respond(voice_data)
+
+time.sleep(1)
+assistant_speak('How can I help you?')
+while 1:
+  voice_data = record_audio()
+  respond(voice_data)
 
     
 
